b7a9b9b5e289327b8fb1f14b4ebae8eda710e61d
Masataro Asai <guicho2.71828@gmail.com>
2022-12-02 09:53:32 -0500

    test various stuff

--- experiments.sh
+++ experiments.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 
-python run.py -d 3 -w 10 -l 100 -- rm -r
+# python run.py -d 3 -w 10 -l 100 -- "rm -r ARG"
+# python run.py -d 3 -w 10 -l 100 -- "find ARG -exec rm {} \;"
+# python run.py -d 3 -w 10 -l 100 -- "find ARG -delete"
+# python run.py -d 3 -w 10 -l 100 -- "mkdir empty; rsync --delete empty/ ARG/ ; rmdir empty"
+python run.py -d 3 -w 10 -l 100 -- "cd ARG; perl -e 'for(<*>){((stat)[9]<(unlink))}'"
+python run.py -d 3 -w 10 -l 100 -- "cd ARG; perl -e 'for(<*>){unlink}'"
 
